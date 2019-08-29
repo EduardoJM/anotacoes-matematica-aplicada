21d7f0f95104b6b0b8b60d8c75d9ccec6492f5e1
Eduardo <eduardo_y05@outlook.com>
2019-08-29 16:05:22 -0300

    Adicionados arquivos R de aulas laboratório

---
+++ src/r/Aula 1.r
@@ -0,0 +1,40 @@
+# Exemplo 1
+
+(0.003815+1234567890)-1234567890
+0.003815+(1234567890-1234567890)
+
+# Exemplo 2
+
+x1<-0.003815
+x2<-1234567890
+
+
+Resultado1<-(x1+x2)-x2
+Resultado1
+
+Resultado2<-x1+(x2-x2)
+Resultado2
+
+# Exemplo 3
+
+x1<-0.3815
+x2<-1234567890
+
+
+Resultado1<-(x1+x2)-x2
+Resultado1
+
+Resultado2<-x1+(x2-x2)
+Resultado2
+
+# Exemplo 4
+
+x1<-0.00000003815
+x2<-1234567890
+
+
+Resultado1<-(x1+x2)-x2
+Resultado1
+
+Resultado2<-x1+(x2-x2)
+Resultado2
